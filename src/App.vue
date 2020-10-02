bbf5bc4dcffbc3fafa83eaf7abbde585a7f8d98b
Phi Dinh <phi.dinh@cloudteam@gmail.com>
2020-10-02 14:16:17 +0700

    Vue html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="wrapper clearfix">
+        
+        <Players />
+        <Control />
+        <Dice />
+
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Players from './components/Player'
+import Control from './components/Control'
+import Dice from './components/Dice'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        Players,
+        Control,
+        Dice
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
+
+.clearfix::after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+body {
+    background-image: linear-gradient(
+        rgba(62, 20, 20, 0.4),
+        rgba(62, 20, 20, 0.4)
+        ),
+        url("/assets/back.jpg");
+    background-size: cover;
+    background-position: center;
+    font-family: Lato;
+    font-weight: 300;
+    position: relative;
+    height: 100vh;
+    color: #555;
+}
+
+.wrapper {
+    width: 1000px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+    overflow: hidden;
+}
+
 </style>
---
+++ src/components/Player.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="wrapper-player">
+        <div class="player-panel winner">
+            <div class="player-name">Winner</div>
+            <div class="player-score">43</div>
+            <div class="player-current-box">
+            <div class="player-current-label">Current</div>
+            <div class="player-current-score">11</div>
+            </div>
+        </div>
+
+        <div class="player-panel">
+            <div class="player-name">Player 2</div>
+            <div class="player-score">72</div>
+            <div class="player-current-box">
+            <div class="player-current-label">Current</div>
+            <div class="player-current-score">0</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'players',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+.player-panel {
+    width: 50%;
+    float: left;
+    height: 600px;
+    padding: 100px;
+    transition: all 0.3s ease;
+    background-color: #fff;
+}
+.player-name {
+    font-size: 40px;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-weight: 100;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    position: relative;
+}
+
+.player-score {
+    text-align: center;
+    font-size: 80px;
+    font-weight: 100;
+    color: #42b983;
+    margin-bottom: 130px;
+}
+
+.active {
+    background-color: #f7f7f7;
+}
+.active .player-name {
+    font-weight: 300;
+}
+
+.active .player-name::after {
+    content: "\2022";
+    font-size: 47px;
+    position: absolute;
+    color: #42b983;
+    top: -7px;
+    right: 10px;
+}
+
+.player-current-box {
+  background-color: #42b983;
+  color: #fff;
+  width: 40%;
+  margin: 0 auto;
+  padding: 12px;
+  text-align: center;
+}
+
+.player-current-label {
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  font-size: 12px;
+  color: #222;
+}
+
+.player-current-score {
+  font-size: 30px;
+}
+
+.winner {
+  background-color: #f7f7f7;
+}
+.winner .player-name {
+  font-weight: 300;
+  color: #42b983;
+}
+
+</style>
